f400ca422eb10a9f7f9521ac5798df5c144442f5
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-04-25 03:00:19 +0600

    databse created

--- lib/db/note_database.dart
+++ lib/db/note_database.dart
@@ -1,3 +1,4 @@
+import 'package:local_storage/model/note.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import '../db/note_database.dart';
@@ -6,6 +7,50 @@ class NoteDatabase {
   // instance that besically calling the constractur
   static final NoteDatabase instance = NoteDatabase._init();
 
+  static Database? _database;
+
 // creating a private constractor
   NoteDatabase._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDb('notes.db');
+    return _database!;
+  }
+
+  // this function will initiat the databse
+
+  Future<Database> _initDb(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  // this function will generate the database
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const boolType = 'BOOLEAN NOT NULL';
+    const integerType = 'INTEGER NOT NULL';
+    const textType = 'TEXT NOT NULL';
+    await db.execute('''
+  CREATE TABLE $tableNotes(
+    ${NoteFields.id} $idType,
+    ${NoteFields.isImportant} $boolType,
+    ${NoteFields.number} $integerType,
+    ${NoteFields.titile} $textType,
+    ${NoteFields.description} $textType,
+    ${NoteFields.time} $textType,
+
+
+  )
+''');
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
 }
--- lib/model/note.dart
+++ lib/model/note.dart
@@ -1,1 +1,28 @@
-class Note {}
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+final String tableNotes = 'notes';
+
+class NoteFields {
+  static final String id = '_id';
+  static final String isImportant = 'isImportant';
+  static final String number = 'number';
+  static final String titile = 'title';
+  static final String description = 'description';
+  static final String time = 'time';
+}
+
+class Note {
+  final int? id;
+  final bool isImportant;
+  final int number;
+  final String title;
+  final String description;
+  final DateTime createdTime;
+  const Note({
+    this.id,
+    required this.isImportant,
+    required this.number,
+    required this.title,
+    required this.description,
+    required this.createdTime,
+  });
+}
