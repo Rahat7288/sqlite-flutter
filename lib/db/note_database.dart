3ea1b9700b82bf829c9321691cdd0a89f1e58a33
Rahat Islam Akash <rahatislamakash@gmail.com>
2023-04-25 02:23:46 +0600

    setting up the directory for sqflite learning

---
+++ lib/db/note_database.dart
@@ -0,0 +1,11 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import '../db/note_database.dart';
+
+class NoteDatabase {
+  // instance that besically calling the constractur
+  static final NoteDatabase instance = NoteDatabase._init();
+
+// creating a private constractor
+  NoteDatabase._init();
+}
